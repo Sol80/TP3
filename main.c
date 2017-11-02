9189c2158c8a1d0ccbf5efa00d7bda90f9030a02
Sol80 <32999367+Sol80@users.noreply.github.com>
2017-11-02 17:11:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,10 +7,29 @@
 
 int main()
 {
-    creationElement();
     T_Liste *l1;
-    l1=creerListe();
-    printf("Liste : la taille est %d, valeur tete %d, valeur queue %d \n",l1->taille,l1->tete, l1->queue);
-    printf("Hello world!\n");
-    return 0;
+    int choix;
+
+    do
+    {
+        printf("\n\n-------------- Actions : ---------------\n");
+        printf("\t1. Creer une liste\n");
+        printf("\t2. Ajouter element liste\n");
+        printf("\t5. Afficher liste\n");
+        printf("\t6. Rechercher element\n");
+        printf("\t8. Quitter\n");
+
+        printf("Entrer votre choix : ");
+        scanf("%d", &choix);
+
+        switch(choix){
+            case 1 : l1 = creerListe(); break;
+            case 2 : insererElement(l1); break; //Faire affichage Echec/Erreur
+            case 5 : afficherListe(l1); break;
+            case 6 : Rechercher ();
+            case 8 : break; //on va quitter <3
+            default : break;
+        }
+    } while (choix != 8);
 }
+
