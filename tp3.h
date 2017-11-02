2006d0f8608f28334ce18c55fa930e4740d7725c
Sol80 <32999367+Sol80@users.noreply.github.com>
2017-11-02 17:12:32 +0100

    Update tp3.h

--- tp3.h
+++ tp3.h
@@ -1,22 +1,22 @@
 #define Nmax 20
 
 typedef struct Element {
-
-char valeur[Nmax];
-int*suiv;
-int*prec;
-
+    char valeur[Nmax];
+    struct Element*suiv;
+    struct Element*prec;
 }T_Element;
 
 typedef struct Liste {
-
-int taille;
-int*tete;
-int*queue;
-
+    int taille;
+    T_Element*t;
+    T_Element*q;
 }T_Liste;
 
 T_Element *creerElement (char *val);
 T_Element *creationElement() ;
 T_Liste *creerListe();
+int insererElement(T_Liste *list);
+void afficherListe(T_Liste *l);
+T_Element *rechercherElement (T_Liste * list, char * val);
+void Rechercher ();
 
