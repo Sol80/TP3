fc7652624ba339379b4cf56319e01edb037e375a
Sol80 <32999367+Sol80@users.noreply.github.com>
2017-11-08 23:42:57 +0100

    Update main.c

--- main.c
+++ main.c
@@ -19,8 +19,11 @@ int main()
         printf("\n\n-------------- Actions : ---------------\n");
         printf("\t1. Creer une liste\n");
         printf("\t2. Ajouter element liste\n");
+        printf("\t3. Supprimer un element d une liste\n");
+        printf("\t4. Rechercher element\n");
         printf("\t5. Afficher liste\n");
-        printf("\t6. Rechercher element\n");
+        printf("\t6. Supprimer une liste\n");
+        printf("\t7. \n");
         printf("\t8. Quitter\n");
 
         printf("Entrer votre choix : ");
@@ -29,8 +32,10 @@ int main()
         switch(choix){
             case 1 : tab1 = creationListe(); list1 = tab1->liste; table1->Tableau[i]=tab1; i++; break; //on cree une liste et on l ajoute dans le tableau des listes
             case 2 : insererElement(list1); break; //Faire affichage Echec/Erreur
+            case 3 : suppressionElement(table1);break;
             case 5 : afficherListe(list1); break;
-            case 6 : Rechercher (table1);
+            case 4 : Rechercher (table1);break;
+            case 6 : suppressionListe(table1);break;
             case 8 : break; //pour quitter
             default : break;
         }
